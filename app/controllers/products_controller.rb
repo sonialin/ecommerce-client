b81ebab6c1e4b5a4442c7409375aee9d25a62e05
Sonia Lin <sonia_lin@hotmail.com>
2016-11-29 15:49:16 -0600

    Added one-click buy with productname and productprice as request params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def buy
+    @result = HTTParty.post('http://localhost:8082/orderservice/order', 
+      :body => {:username => 'sonialin', :productname => params[:productname], :productqty => 1, :amount => params[:amount]}.to_json, 
+      :headers => { 'Content-Type' => 'application/json' })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
   root :to => "products#index"
+  match "products/buy" => "products#buy", as: :products_buy, via: :post
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
